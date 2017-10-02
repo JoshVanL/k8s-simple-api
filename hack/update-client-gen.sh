59ad2a407ae8c498f27cfc0650b05ee4330aaa2a
JoshVanL <jv15626@my.bristol.ac.uk>
2017-10-02 16:40:08 +0100

    Use variable in script

--- hack/update-client-gen.sh
+++ hack/update-client-gen.sh
@@ -6,35 +6,36 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO_ROOT=$GOPATH/src/github.com/joshvanl/k8s-simple-api
+PACKAGE=github.com/joshvanl/k8s-simple-api
+REPO_ROOT=$GOPATH/src/${PACKAGE}
 BINDIR=${REPO_ROOT}/bin
 
 # Generate the internal clientset (pkg/client/clientset_generated/internalclientset)
 ${BINDIR}/client-gen "$@" \
-	      --input-base "github.com/joshvanl/k8s-simple-api/pkg/apis/" \
+	      --input-base "${PACKAGE}/pkg/apis/" \
 	      --input "simple/" \
-	      --clientset-path "github.com/joshvanl/k8s-simple-api/pkg/client/" \
+	      --clientset-path "${PACKAGE}/pkg/client/" \
 	      --clientset-name internalclientset \
 	      --go-header-file "${GOPATH}/src/github.com/kubernetes/repo-infra/verify/boilerplate/boilerplate.go.txt"
 # Generate the versioned clientset (pkg/client/clientset_generated/clientset)
 ${BINDIR}/client-gen "$@" \
-		  --input-base "github.com/joshvanl/k8s-simple-api/pkg/apis/" \
+		  --input-base "${PACKAGE}/pkg/apis/" \
 		  --input "simple/v1alpha1" \
-	      --clientset-path "github.com/joshvanl/k8s-simple-api/pkg/" \
+	      --clientset-path "${PACKAGE}/pkg/" \
 	      --clientset-name "client" \
 	      --go-header-file "${GOPATH}/src/github.com/kubernetes/repo-infra/verify/boilerplate/boilerplate.go.txt"
 # generate lister
 ${BINDIR}/lister-gen "$@" \
-		  --input-dirs="github.com/joshvanl/k8s-simple-api/pkg/apis/simple" \
-	      --input-dirs="github.com/joshvanl/k8s-simple-api/pkg/apis/simple/v1alpha1" \
-	      --output-package "github.com/joshvanl/k8s-simple-api/pkg/listers" \
+		  --input-dirs="${PACKAGE}/pkg/apis/simple" \
+	      --input-dirs="${PACKAGE}/pkg/apis/simple/v1alpha1" \
+	      --output-package "${PACKAGE}/pkg/listers" \
 	      --go-header-file "${GOPATH}/src/github.com/kubernetes/repo-infra/verify/boilerplate/boilerplate.go.txt"
 # generate informer
 ${BINDIR}/informer-gen "$@" \
 	      --go-header-file "${GOPATH}/src/github.com/kubernetes/repo-infra/verify/boilerplate/boilerplate.go.txt" \
-	      --input-dirs "github.com/joshvanl/k8s-simple-api/pkg/apis/simple" \
-	      --input-dirs "github.com/joshvanl/k8s-simple-api/pkg/apis/simple/v1alpha1" \
-	      --internal-clientset-package "github.com/joshvanl/k8s-simple-api/pkg/client/internalclientset" \
-	      --versioned-clientset-package "github.com/joshvanl/k8s-simple-api/pkg/client" \
-	      --listers-package "github.com/joshvanl/k8s-simple-api/pkg/listers" \
-	      --output-package "github.com/joshvanl/k8s-simple-api/pkg/informers"
+	      --input-dirs "${PACKAGE}/pkg/apis/simple" \
+	      --input-dirs "${PACKAGE}/pkg/apis/simple/v1alpha1" \
+	      --internal-clientset-package "${PACKAGE}/pkg/client/internalclientset" \
+	      --versioned-clientset-package "${PACKAGE}/pkg/client" \
+	      --listers-package "${PACKAGE}/pkg/listers" \
+	      --output-package "${PACKAGE}/pkg/informers"
